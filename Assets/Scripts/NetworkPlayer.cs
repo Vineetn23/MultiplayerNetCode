7589dc3ab7fec5e0b51f7058180e40048544b2cc
Vineetn23 <vineetn23@gmail.com>
2023-10-07 15:45:48 +0530

    VR Network and Animation Sync

---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class NetworkPlayer : NetworkBehaviour
+{
+    public Transform root;
+    public Transform head;
+    public Transform leftHand;
+    public Transform rightHand;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (IsOwner)
+        {
+            root.position = VRRigReferences.Singleton.root.position;
+            root.rotation = VRRigReferences.Singleton.root.rotation;
+
+            head.position = VRRigReferences.Singleton.head.position;
+            head.rotation = VRRigReferences.Singleton.head.rotation;
+
+            leftHand.position = VRRigReferences.Singleton.leftHand.position;
+            leftHand.rotation = VRRigReferences.Singleton.leftHand.rotation;
+
+            rightHand.position = VRRigReferences.Singleton.rightHand.position;
+            rightHand.rotation = VRRigReferences.Singleton.rightHand.rotation;
+        }
+    }
+}
