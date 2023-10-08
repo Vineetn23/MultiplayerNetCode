36110f8b90ab9018a25b66f05b59a7bb650ea089
Vineetn23 <vineetn23@gmail.com>
2023-10-08 19:15:21 +0530

    Added Lobby and LobbyManager

---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+
+public class LobbyManager : MonoBehaviour
+{
+    public static LobbyManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public struct LobbyData
+    {
+        public string lobbyName;
+        public int maxPlayer;
+    }
+    public async void CreateLobby(LobbyData lobbyData)
+    {
+        CreateLobbyOptions lobbyOptions = new CreateLobbyOptions();
+        lobbyOptions.IsPrivate = false;
+        lobbyOptions.Data = new Dictionary<string, DataObject>();
+
+        string joinCode = await RelayManager.Instance.CreateRelayGame(lobbyData.maxPlayer);
+
+
+        DataObject dataObject = new DataObject(DataObject.VisibilityOptions.Public, joinCode);
+        lobbyOptions.Data.Add("Join Code Key", dataObject);
+
+        await Lobbies.Instance.CreateLobbyAsync(lobbyData.lobbyName, lobbyData.maxPlayer, lobbyOptions);
+    }
+
+    public async void QuickJoinLobby()
+    {
+        Lobby lobby = await Lobbies.Instance.QuickJoinLobbyAsync();
+        string relayJoinCode = lobby.Data["Join Code Key"].Value;
+
+        RelayManager.Instance.JoinRelayGame(relayJoinCode);
+    }
+
+    public async void JoinLobby(string lobbyId)
+    {
+        Lobby lobby = await Lobbies.Instance.JoinLobbyByIdAsync(lobbyId);
+        string relayJoinCode = lobby.Data["Join Code Key"].Value;
+
+        RelayManager.Instance.JoinRelayGame(relayJoinCode);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/RelayManager.cs
+++ Assets/Scripts/RelayManager.cs
@@ -6,6 +6,7 @@ using Unity.Netcode.Transports.UTP;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using Unity.Networking.Transport.Relay;
+using System.Threading.Tasks;
 
 public class RelayManager : MonoBehaviour
 {
@@ -23,7 +24,7 @@ public class RelayManager : MonoBehaviour
         transport = GetComponent<UnityTransport>();
     }
 
-    public async void CreateRelayGame(int maxPlayer)
+    public async Task<string> CreateRelayGame(int maxPlayer)
     {
         Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxPlayer);
 
@@ -33,9 +34,15 @@ public class RelayManager : MonoBehaviour
 
         transport.SetHostRelayData(allocation.RelayServer.IpV4, (ushort)allocation.RelayServer.Port,  allocation.AllocationIdBytes, allocation.Key, allocation.ConnectionData);
 
+        //RelayServerData serverData = new RelayServerData(allocation, "dtls");
+        //transport.SetRelayServerData(serverData);
         NetworkManager.Singleton.StartHost();
 
+        return joinCode;
     }
+
+
+   
 
     public async void JoinRelayGame(string joinCode)
     {
